96aa821a57919a4529acc93fd0de2b9f1de14d06
Sakuya <zx.l@bozee.cn>
2026-09-24 18:30:04 +0800

    chore: github page 适配

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
-export default defineConfig({ plugins: [vue(), UnoCSS()] })
+export default defineConfig({
+    plugins: [vue(), UnoCSS()],
+    base: process.env.GITHUB_ACTIONS ? '/gcfp-b30/' : '/',
+})
